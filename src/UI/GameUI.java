58ffb0cf90bf8431eee30a5d2f3eac4f07c3ce5e
MrPugken <pablogot.comic@gmail.com>
2026-05-13 10:34:18 -0600

    Summary

---
+++ src/UI/GameUI.java
@@ -0,0 +1,24 @@
+package UI;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class GameUI {
+	
+	ImageIcon uI = new ImageIcon("SSSBackgrounds.png");
+	Image top;
+	
+	public GameUI() {
+		
+		top = uI.getImage();
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		g.drawImage(top, 10, 10, 10, 10, this);
+	}
+}
