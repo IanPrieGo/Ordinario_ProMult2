9aaf870ffc3bdf030a6dbf11df0033a7ef892b96
MrPugken <pablogot.comic@gmail.com>
2026-05-13 22:25:10 -0600

    NewImageStructure

--- src/UI/GameUI.java
+++ src/UI/GameUI.java
@@ -1,23 +1,40 @@
 package UI;
 
 import java.awt.*;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.event.*;
+import finalGame.GameObject;
+import finalGame.GamePanel;
+import finalGame.KeyHandler;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 
-public class GameUI extends JPanel{
+public class GameUI extends GameObject{
 	
 	ImageIcon uI = new ImageIcon("src\\Resourses\\SSSBackgrounds.png");
 	Image top;
+	BufferedImage bar;
 	
-	public GameUI() {
+	String topBar = "src\\Resourses\\SSSBackgrounds.png";
+	
+	public GameUI (GamePanel gp, KeyHandler keyH) {
+		super(gp, keyH);
 		
-		top = new ImageIcon("src\\Resourses\\SSSBackgrounds.png").getImage();
+		try {
+			bar = ImageIO.read( new File (topBar));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
+	public void draw(Graphics2D g2) {
 		
-		g.drawImage(top, 10, 10, 10, 10, this);
+		Image sprite = bar.getScaledInstance(20, 20, 0);
+		
+		g2.drawImage(sprite, 0, 0, Color.red, null);
+		
 	}
 }
